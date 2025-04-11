eb291cbd3079e54836e402357753711543344c33
Thibault Brevet <hi@aatb.ch>
2025-04-11 10:33:38 +0200

    pre-PR lint

--- mujoco_playground/config/locomotion_params.py
+++ mujoco_playground/config/locomotion_params.py
@@ -88,8 +88,8 @@ def brax_ppo_config(env_name: str) -> config_dict.ConfigDict:
         policy_obs_key="state",
         value_obs_key="privileged_state",
     )
-  
-  elif env_name in ("Go2JoystickFlatTerrain","Go2JoystickRoughTerrain"):
+
+  elif env_name in ("Go2JoystickFlatTerrain", "Go2JoystickRoughTerrain"):
     rl_config.num_timesteps = 200_000_000
     rl_config.num_evals = 10
     rl_config.num_resets_per_eval = 1
